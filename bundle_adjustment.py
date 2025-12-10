031d377afc0c57b73692b03eaf622ebfa50b66e7
Alice Liu <aliceliu@college.harvard.edu>
2025-12-10 18:21:01 -0500

    Changed bundle_adjustment code

--- bundle_adjustment.py
+++ bundle_adjustment.py
@@ -15,8 +15,26 @@ optim + gradients + other stuff
 '''
 
 import torch
+import cv2
+import numpy as np
 
-def bundle_adjustment(scene_points, camera_poses, camera_matrix, img_points):
+# Project 3D points to 2D using camera parameters and 3D points for ONE camera
+def project_points(scene_points, camera_rotation, camera_translation, camera_matrix, distCoeffs):
+    # using cv2.projectPoints
+    projected_points = []
+    projected_points.append(cv2.projectPoints(scene_points, camera_rotation, camera_translation, camera_matrix, distCoeffs)[0])
+    return torch.stack(projected_points, dim=0)
+
+def compute_camera_matrix(focal_length, principal_point_x, principal_point_y):
+    camera_matrix = torch.zeros((3, 3), dtype=torch.float32)
+    camera_matrix[0, 0] = focal_length
+    camera_matrix[1, 1] = focal_length
+    camera_matrix[0, 2] = principal_point_x
+    camera_matrix[1, 2] = principal_point_y
+    camera_matrix[2, 2] = 1.0
+    return camera_matrix
+
+def bundle_adjustment(scene_points, camera_rotations, camera_translations, camera_matrix, img_points, distCoeffs):
     loss = torch.nn.MSELoss()
     
     '''
@@ -28,28 +46,49 @@ def bundle_adjustment(scene_points, camera_poses, camera_matrix, img_points):
     Might need to make some things static (ex. First camera)
     '''
     # Set up learnable parameters (?)
-    scene_points = torch.nn.Parameter(scene_points)
-    camera_poses = torch.nn.Parameter(camera_poses)
-    camera_matrix = torch.nn.Parameter(camera_matrix)
+    # Write a helper function to construct the camera matrix/principal point from parameters
+    # Have focal length as it's own parameter 
+    # Question: only turn on ability to optimize the camera matrix for the extension
+    focal_length = torch.nn.Parameter(torch.tensor([camera_matrix[0,0]], dtype=torch.float32), requires_grad=True)
+    principal_point_x = torch.nn.Parameter(torch.tensor([camera_matrix[0,2]], dtype=torch.float32), requires_grad=True)
+    principal_point_y = torch.nn.Parameter(torch.tensor([camera_matrix[1,2]], dtype=torch.float32), requires_grad=True)
 
+    # Paramaterize 3D points
+    scene_points = torch.nn.Parameter(scene_points, requires_grad=True)
+
+    # Paramaterize translations
+    camera_translations = torch.nn.Parameter(camera_translations, requires_grad=True)
+
+    # Convert camera_rotations to axis-angle (Rodrigues) form before parameterizing
+    camera_rotations = cv2.Rodrigues(camera_rotations.detach().numpy())[0]
+    camera_rotations = torch.nn.Parameter(torch.tensor(camera_rotations, dtype=torch.float32), requires_grad=True)
 
     # TODO: not sure where to put this?
-    camera_mask = torch.ones(camera_poses.shape[0], 1, dtype=torch.float32)
+    camera_mask = torch.ones(camera_rotations.shape[0], 1, dtype=torch.float32)
     camera_mask[0] = 0. # Fix the first camera pose
 
     # TODO: fix this parameter passing in depending on how the parameters are set up
-    optimizer = torch.optim.Adam(list(scene_points.parameters()) + list(camera_poses.parameters()), lr=1e-3)
-    num_iterations = 1000
+    optimizer = torch.optim.Adam([scene_points, camera_rotations, camera_translations, focal_length, principal_point_x, principal_point_y], lr=1e-3)
+    # TODO: Increase num_iterations after we verify this works
+    num_iterations = 100
 
-    print("Initial loss before bundle adjustment:", loss(project_points(scene_points, camera_poses, camera_matrix), img_points).item())
+
+    print("Initial loss before bundle adjustment:", loss(project_points(scene_points, camera_rotations, camera_translations, camera_matrix, distCoeffs), img_points).item())
 
     for iter in range(num_iterations):
         optimizer.zero_grad()
 
-        # TODO: change project points to whatever function we have that computes the image points (?)
-        projected_points = project_points(scene_points, camera_poses, camera_matrix)
+        # Recompute camera matrix from parameters
+        camera_matrix = compute_camera_matrix(focal_length, principal_point_x, principal_point_y)
 
-        current_loss = loss(projected_points, img_points)
+        current_loss = 0.0
+        # Compute loss over all cameras (that are not the first one)
+        for i in range(1, camera_rotations.shape[0]):
+            # Compute matrix form of camera rotation
+            rotation_matrix = cv2.Rodrigues(camera_rotations[i].detach().numpy())[0]
+            # Compute projected points and loss
+            projected_points = project_points(scene_points, rotation_matrix, camera_translations[i], camera_matrix, distCoeffs)
+            current_loss += loss(projected_points, img_points[i]) * camera_mask[i]
 
         current_loss.backward()
         optimizer.step()
@@ -59,5 +98,11 @@ def bundle_adjustment(scene_points, camera_poses, camera_matrix, img_points):
             print(f"Iteration {iter}, Loss: {current_loss.item()}")
     
     print("Final loss after bundle adjustment:", current_loss.item())
-    
-    return scene_points, camera_poses, camera_matrix
+
+    # Convert all parameters back to numpy arrays for output
+    scene_points = scene_points.detach().numpy()
+    camera_rotations = cv2.Rodrigues(camera_rotations.detach().numpy())[0]
+    camera_translations = camera_translations.detach().numpy()
+    camera_matrix = compute_camera_matrix(focal_length.item(), principal_point_x.item(), principal_point_y.item()).detach().numpy()
+
+    return scene_points, camera_rotations, camera_translations, camera_matrix
